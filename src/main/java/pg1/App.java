506d1f7f930118611dbade5cda969bfd361fce60
UdayShree123 <udayshree123@gmail.com>
2019-06-07 12:52:34 +0530

    1

---
+++ src/main/java/pg1/App.java
@@ -0,0 +1,18 @@
+package pg1;
+
+public class App 
+{
+	public static void main(String args[])
+    {
+        int num = 9;
+        while(num > 0)
+        {
+            for(int i=1; i<=num; i++)
+            {
+                System.out.print(" "+num+" ");
+            }
+            System.out.print("\n");
+            num--;
+        }
+    }
+}
